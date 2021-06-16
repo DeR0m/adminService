14491576bf1e5ddce916745582ae58a19eed4362
DeR0m <denisromantsov@mail.ru>
2021-06-16 20:26:54 +0300

    add flight service

---
+++ src/main/java/com/example/adminService/service/FlightService.java
@@ -0,0 +1,32 @@
+package com.example.adminService.service;
+
+import com.example.adminService.domain.Flights;
+import com.example.adminService.repo.FlightsRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FlightService {
+
+    @Autowired
+    private FlightsRepo flightsRepo;
+
+    public Iterable<Flights> getFlights() {
+        return flightsRepo.findAll();
+    }
+
+    public Flights create(Flights flights) {
+        return flightsRepo.save(flights);
+    }
+
+    public Flights update(Flights flightsForDb, Flights flights) {
+        BeanUtils.copyProperties(flights, flightsForDb, "id");
+        return flightsRepo.save(flightsForDb);
+    }
+
+    public Long delete(Long id) {
+        flightsRepo.deleteById(id);
+        return id;
+    }
+}
--- src/main/java/com/example/adminService/service/impl/AdminServieImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.adminService.service.impl;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AdminServieImpl {
-}
